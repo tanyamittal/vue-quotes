34cf78a338f0885a81569d1d05c3325e6d0e404c
sbmsc <chauhanshubham089@gmail.com>
2019-03-16 15:12:49 +0530

    adding project

---
+++ vue-udemy/Start/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes "></app-header>
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on the box to delete it.</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
+import Header from "./components/Header.vue";
+export default {
+  data: function() {
+    return {
+      quotes: ["Just a quote"],
+      maxQuotes: 10
+    };
+  },
+  methods: {
+    newQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert(
+          "Maximum number of quotes achieved. Delete before adding a new one."
+        );
+      }
+      console.log("newQuote in app.vue" + quote);
+      this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      console.log("deletequote in app.vue" + index);
+      this.quotes.splice(index, 1);
+    }
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ vue-udemy/Start/src/components/NewQuote.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <form>
+        <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+          <label for="new">Quote</label>
+          <textarea name="new" id="newQuote" cols="30" rows="3" v-model="quote"></textarea>
+        </div>
+        <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
+          <button class="btn btn-primary" @click.prevent="newQuote">Add Quote</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data: function() {
+    return {
+      quote: ""
+    };
+  },
+  methods: {
+    newQuote() {
+      this.$emit("quoteAdded", this.quote);
+      this.quote = "";
+    }
+  }
+};
+</script>
+<style scoped>
+textarea {
+  margin-top: 20px;
+}
+</style>
